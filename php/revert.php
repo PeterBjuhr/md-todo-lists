ba0173906eec0ff31df013c97c5aca235280ee38
PeterBjuhr <peterbjuhr@gmail.com>
2015-09-23 21:49:11 +0200

    Adding new sync dir to revert

--- php/revert.php
+++ php/revert.php
@@ -2,7 +2,7 @@
 $file = $_POST['file'];
 $xtra = $_POST['xtracheck'];
 if($xtra==5){
-	echo file_get_contents($file);
+	echo file_get_contents('../synced/' . $file);
 }else{
 	echo "Unpermitted violation!!!";
 }
